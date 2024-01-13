27557f78fe2849338c7a0cf33282665bc1fdce30
DJ-Greenwood <Denzil.James.greenwood@gmail.com>
2024-01-12 19:28:46 -0600

    Update TestAI.py
    fixed the .gitignore file

--- myflaskapp/myflaskapp/TestAI.py
+++ myflaskapp/myflaskapp/TestAI.py
@@ -5,9 +5,10 @@ from StoryPackage import StoryDatabase
 # Now you can use StoryBotAI
 bot = StoryBotAI(model="gpt-3.5-turbo-1106")
 
-db = StoryDatabase('myflaskapp\StoryPackage\StoryDatabase.db')
+
+#db = StoryDatabase('myflaskapp\StoryPackage\StoryDatabase.db')
 
 
-db.create_table_from_json('myflaskapp\StoryPackage\JsonFiles\story_elements.json')
-
-
+#db.create_table_from_json('myflaskapp\StoryPackage\JsonFiles\story_elements.json')
+a = bot.get_chatgpt_actor_one(system_prompt= "you are a master writer that uses dialogue to tell extravagen stories", prompt="write a story for me.")
+print(a)
